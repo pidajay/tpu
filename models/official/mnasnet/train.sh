53896ce8af1e7a0623fb90f94ccd25441458724b
Ajay P <paiajay@amazon.com>
2019-07-16 23:20:37 -0700

    Modified train.sh

--- models/official/mnasnet/train.sh
+++ models/official/mnasnet/train.sh
@@ -1,4 +1,4 @@
-~/anaconda3/envs/tensorflow_p36/bin/mpirun -np 8 -hostfile hosts.txt -mca plm_rsh_no_tree_spawn 1 \
+mpirun -np 8 -hostfile hosts.txt -mca plm_rsh_no_tree_spawn 1 \
         -bind-to socket -map-by slot \
         -x HOROVOD_HIERARCHICAL_ALLREDUCE=1 -x HOROVOD_FUSION_THRESHOLD=16777216 \
         -x NCCL_MIN_NRINGS=4 -x LD_LIBRARY_PATH -x PATH -mca pml ob1 -mca btl ^openib \
