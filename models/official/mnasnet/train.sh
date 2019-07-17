c74fd889fb1c8613b57b024f406337ac9bb26ab7
Ajay P <paiajay@amazon.com>
2019-07-16 23:16:08 -0700

    Modified train.sh

--- models/official/mnasnet/train.sh
+++ models/official/mnasnet/train.sh
@@ -4,4 +4,4 @@
         -x NCCL_MIN_NRINGS=4 -x LD_LIBRARY_PATH -x PATH -mca pml ob1 -mca btl ^openib \
         -x NCCL_SOCKET_IFNAME=ens5 -mca btl_tcp_if_exclude lo,docker0 \
         -x TF_CPP_MIN_LOG_LEVEL=0 \
-        python mnasnet_main_hvd.py --use_tpu=False --data_dir=/home/ubuntu/data --model_dir=./results_hvd --train_batch_size=256 --eval_batch_size=256 --skip_host_call=False --data_format='channels_first' --transpose_input=False --use_horovod=True
+        python mnasnet_main_hvd.py --use_tpu=False --data_dir=/home/ubuntu/data --model_dir=./results_hvd --train_batch_size=256 --eval_batch_size=256 --train_steps=218949 --skip_host_call=False --data_format='channels_first' --transpose_input=False --use_horovod=True
